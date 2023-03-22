67462b0c496a8b83c7bb44724058aa34635a10f5
SofiaCurreri <sofiacurreri@gmail.com>
2023-03-22 23:24:46 +0100

    classe Movie + istanze + function construct

--- index.php
+++ index.php
@@ -1,17 +1,36 @@
-<?php ?>
+<?php
 
-<!DOCTYPE html>
-<html lang="en">
+//Create un file index.php in cui:
+ //- è definita una classe 'Movie'
+ //=> all'interno della classe sono dichiarate delle variabili d'istanza
+ //=> all'interno della classe è definito un costruttore
+ //=> all'interno della classe è definito almeno un metodo
+//- vengono istanziati almeno due oggetti 'Movie' e stampati a schermo i valori delle relative proprietà
+//Bonus 1:
+//Modificare la classe Movie in modo che accetti piú di un genere.
+//Bonus 2:
+//Creare un layout completo per stampare a schermo una lista di movies.
+//Facciamo attenzione all'organizzazione del codice, suddividendolo in appositi file e cartelle. Possiamo ad esempio organizzare il codice
+//- creando un file dedicato ai dati (tipo le array di oggetti) che potremmo chiamare db.php
+//- mettendo ciascuna classe nel proprio file e magari raggruppare tutte le classi in una cartella dedicata che possiamo chiamare Models/
+//- organizzando il layout dividendo la struttura ed i contenuti in file e parziali dedicati.
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Primo Esercizio OOP</title>
-</head>
+class Movie {
+    public $title;
+    public $length;
+    public $rating;
 
-<body>
+    function __construct(
+        string $_title,
+        string $_length,
+        float $_rating
+    )
+    {
+      $this -> title = $_title, 
+      $this -> length = $_length,
+      $this -> rating = $_rating, 
+    }
 
-</body>
+}
 
-</html>
+?>
